b70e9c6f30ada1284e0006cf11e459f31d12011a
Martietamis <martinetamis@gmail.com>
2016-10-06 00:15:13 -0400

    Ajoutez des fichiers projet.

---
+++ Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        System.Diagnostics.EventLog.WriteEntry("AnnoyingTord3",
+        "Just an update Tord! Message successfully Transformed");
+
+    }
+}
